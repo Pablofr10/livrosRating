f7925ad2ce412ee5f6538a5396087f4478c9c12b
Pablofr10 <rafanet.live@gmail.com>
2021-01-24 13:57:26 -0300

    Adicionando funcionalidade de assistir mais tarde.

--- src/Tela1.js
+++ src/Tela1.js
@@ -19,6 +19,7 @@ const Tela1 = () => {
       ano: 2020,
       estilo: 'Comédia',
       direcao: 'Tony Cervone',
+      assistirDepois: false,
       image:
         'https://br.web.img3.acsta.net/pictures/20/03/05/20/58/4942195.jpg',
       descricao:
@@ -26,20 +27,48 @@ const Tela1 = () => {
     },
     {
       key: '1',
-      filme: 'Harry Potter e o Prisioneiro de Askaban',
+      filme: 'Harry Potter',
       ano: 2020,
       estilo: 'Aventura',
-      direcao: 'J.K. Rolling',
+      direcao: 'Alfonso Cuarón',
+      assistirDepois: false,
       image:
         'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fbr.web.img3.acsta.net%2Fmedias%2Fnmedia%2F18%2F95%2F59%2F60%2F20417256.jpg&f=1&nofb=1',
       descricao:
-        'Scooby e sua turma encaram o seu maior e mais difÃ­cil mistÃ©rio de todos os tempos: um plano maligno para liberar o cÃ£o fantasma, CÃ©rbero, no mundo. Enquanto corre para impedir esse "apocÃ£olipse" global, a turma descobre que Scooby tem um legado secreto e um destino Ã©pico maior do que qualquer um podia imaginar.',
+        'O 3º ano de ensino na Escola de Magia e Bruxaria de Hogwarts se aproxima. Porém um grande perigo ronda a escola: o assassino Sirius Black (Gary Oldman) fugiu da prisão de Azkaban, considerada até então como à prova de fugas. Para proteger a escola são enviados os Dementadores, estranhos seres que sugam a energia vital de quem se aproxima deles, que tanto podem defender a escola como piorar ainda mais a situação.',
+    },
+    {
+      key: '2',
+      filme: 'Harry Potter',
+      ano: 2020,
+      estilo: 'Aventura',
+      direcao: 'Alfonso Cuarón',
+      assistirDepois: false,
+      image:
+        'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fbr.web.img3.acsta.net%2Fmedias%2Fnmedia%2F18%2F95%2F59%2F60%2F20417256.jpg&f=1&nofb=1',
+      descricao:
+        'O 3º ano de ensino na Escola de Magia e Bruxaria de Hogwarts se aproxima. Porém um grande perigo ronda a escola: o assassino Sirius Black (Gary Oldman) fugiu da prisão de Azkaban, considerada até então como à prova de fugas. Para proteger a escola são enviados os Dementadores, estranhos seres que sugam a energia vital de quem se aproxima deles, que tanto podem defender a escola como piorar ainda mais a situação.',
+    },
+    {
+      key: '3',
+      filme: 'Harry Potter',
+      ano: 2020,
+      estilo: 'Aventura',
+      direcao: 'Alfonso Cuarón',
+      assistirDepois: false,
+      image:
+        'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fbr.web.img3.acsta.net%2Fmedias%2Fnmedia%2F18%2F95%2F59%2F60%2F20417256.jpg&f=1&nofb=1',
+      descricao:
+        'O 3º ano de ensino na Escola de Magia e Bruxaria de Hogwarts se aproxima. Porém um grande perigo ronda a escola: o assassino Sirius Black (Gary Oldman) fugiu da prisão de Azkaban, considerada até então como à prova de fugas. Para proteger a escola são enviados os Dementadores, estranhos seres que sugam a energia vital de quem se aproxima deles, que tanto podem defender a escola como piorar ainda mais a situação.',
     },
   ];
 
   return (
     <View>
-      <Text style={styles.title}>Lista de Filmes</Text>
+      <View>
+        <Text style={styles.title}>Lista de Filmes</Text>
+        <Text style={styles.title}>Lista de Filmes</Text>
+      </View>
       <FlatList
         data={DATA}
         renderItem={({item}) => <FlatComponent data={item} />}
--- src/components/FlatComponent.js
+++ src/components/FlatComponent.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
 
 import {useNavigation} from '@react-navigation/native';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 const FlatComponent = ({data}) => {
   const navigation = useNavigation();
+
+  const [assistirDepois, setAssistirDepois] = useState(false);
 
   const navegar = () => {
     navigation.navigate('Tela2', {data});
   };
 
+  const assistirMaisTarde = () => {
+    alert('Adicionado para assistir mais tarde!');
+    setAssistirDepois(true);
+  };
+
   return (
-    <TouchableOpacity style={styles.container} onPress={navegar}>
+    <View style={styles.container} onPress={navegar}>
       <Image source={{uri: data.image}} style={{width: 100, height: 100}} />
       <View style={styles.subcontainer}>
         <Text style={styles.title}>{data.filme}</Text>
         <Text>{data.ano}</Text>
         <Text>{data.estilo}</Text>
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.btn} onPress={navegar}>
+            <Text style={styles.btnText}>Ver Mais</Text>
+            <Icon name="chevron-right" size={20} color={'#DAD1BF'} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.add} onPress={assistirMaisTarde}>
+            <Icon
+              name={assistirDepois ? 'clock-o' : 'plus'}
+              size={30}
+              color={'#000000'}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
-    </TouchableOpacity>
+    </View>
   );
 };
 
@@ -26,7 +48,10 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#DAD1BF',
     marginBottom: 5,
+    marginLeft: 5,
+    marginRight: 5,
     flexDirection: 'row',
+    borderRadius: 5,
   },
   subcontainer: {
     marginLeft: 10,
@@ -34,6 +59,24 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 18,
     fontWeight: 'bold',
+  },
+  actions: {
+    width: 250,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  btn: {
+    width: 100,
+    height: 40,
+    flexDirection: 'row',
+    backgroundColor: 'black',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  btnText: {
+    color: '#DAD1BF',
+    fontSize: 16,
   },
 });
 
